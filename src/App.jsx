0291d00516894c4da5b4201ea5822d1be97e9595
summermong <2514o_o@naver.com>
2024-05-07 15:31:22 +0900

    510 ~ 511. 컨텍스트 API로 상태관리 및 컴파운드 컴포넌트 그룹화

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Accordion from './components/Accordion/Accordion';
-import AccordionItem from './components/Accordion/AccordionItem';
 
 function App() {
   return (
@@ -7,7 +6,8 @@ function App() {
       <section>
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
-          <AccordionItem
+          <Accordion.Item
+            id="experience"
             className="accordion-item"
             title="We got 20 years of experience"
           >
@@ -18,8 +18,9 @@ function App() {
                 vacation trips for more than 20 years.
               </p>
             </article>
-          </AccordionItem>
-          <AccordionItem
+          </Accordion.Item>
+          <Accordion.Item
+            id="local-guides"
             className="accordion-item"
             title="We're working with local guides"
           >
@@ -30,7 +31,7 @@ function App() {
                 pleasant vacation.
               </p>
             </article>
-          </AccordionItem>
+          </Accordion.Item>
         </Accordion>
       </section>
     </main>
--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,3 +1,37 @@
-export default function Accordion({ children, className }) {
-  return <ul className={className}>{children}</ul>;
+import { useContext, useState } from 'react';
+import { createContext } from 'react';
+import AccordionItem from './AccordionItem';
+
+const AccordionContext = createContext();
+
+export function useAccordionContext() {
+  const ctx = useContext(AccordionContext);
+
+  if (!ctx) {
+    throw new Error(
+      'Accordion-related components must be wrapped by <Accordion>.'
+    );
+  }
+  return ctx;
 }
+
+export default function Accordion({ children, className }) {
+  const [openItemId, setOpenItemId] = useState();
+
+  function toggleItem(id) {
+    setOpenItemId(prevId => (prevId === id ? null : id));
+  }
+
+  const contextValue = {
+    openItemId,
+    toggleItem,
+  };
+
+  return (
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>{children}</ul>
+    </AccordionContext.Provider>
+  );
+}
+
+Accordion.Item = AccordionItem;
